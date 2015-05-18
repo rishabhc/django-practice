8f708178e1dd6b94c4750b396ad04ea12bd4017d
Rishabh Chhabra <rishabh.chhabra561@gmail.com>
2015-05-18 10:50:52 +0530

    Adds URLs for attendanceTracker, changes some settings and removes swap files

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^polls/',include('polls.urls',namespace="polls")),
+    url(r'^attendanceTracker/',include('attendanceTracker.urls',namespace="attendanceTracker")),
     url(r'^admin/', include(admin.site.urls)),
 ]
